3dbcd323b8c91d86b25fa93736505f79ec7f9ff5
chenyuhang01@gmail.com <chenyuhang01@gmail.com>
2022-02-21 21:39:36 +0800

    Initial Commit

---
+++ ReceiptTracker-v2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ReceiptTracker-v2
+//
+//  Created by Chen Yu Hang on 21/2/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ReceiptTracker-v2UITests/ReceiptTracker_v2UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ReceiptTracker_v2UITestsLaunchTests.swift
+//  ReceiptTracker-v2UITests
+//
+//  Created by Chen Yu Hang on 21/2/22.
+//
+
+import XCTest
+
+class ReceiptTracker_v2UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
